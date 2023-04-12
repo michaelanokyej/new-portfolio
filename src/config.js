e71b78e3d43a4fbad3750b58b33b41ae6acd5d7f
michaelanokyej <michaelanokyej@yahoo.com>
2023-04-11 20:46:52 -0400

    updated endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 // export default {
 //   API_ENDPOINT: "https://shielded-eyrie-54741.herokuapp.com/api"
 // };
+// export default {
+//   API_ENDPOINT: "http://localhost:5000/api"
+// };
 export default {
-  API_ENDPOINT: "http://localhost:5000/api"
+  API_ENDPOINT: "https://portfolio-service-tgvp.onrender.com"
 };
